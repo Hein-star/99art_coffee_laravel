ed0a7c2eddf549ac1ed2e5e24c5e4c28083e34aa
Hein-star <name.nm663@gmail.com>
2020-11-23 16:17:13 +0630

    login and create form prepare

--- resources/views/frontend/signin.blade.php
+++ resources/views/frontend/signin.blade.php
@@ -1,7 +1,58 @@
 @extends('frontendtemplate')
-
+<link rel="stylesheet" type="text/css" href="{{asset('frontend_asset/assets/style.css')}}">
 @section('content')
-<div class="img-fluid">
+<div class="bg-img">
+    <div class="content">
+        <header>Login From</header>
+        <form method="POST" action="{{route('login')}}">
+            @csrf
+            <div class="field">
+                <span class="fa fa-user"></span>
+                <input id="email" type="text" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Your Email" required autocomplete="email" autofocus>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="field space">
+                <span class="fa fa-lock"></span>
+                <input id="password" type="password" name="password" class="password @error('password') is-invalid @enderror" autocomplete="current-password" placeholder="Your Password" required>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="pass">
+                <a href="#">Forgot Password?</a>
+            </div>
+            <div>
+                <button type="submit" class="form-control btn btn-primary">
+                    {{ __('Login') }}
+                </button>
+            </div>
+            <div class="link">
+                <div class="facebook">
+                    <a href="https://web.facebook.com/kawe.lay.127/"><i class="fab fa-facebook-f"><span>Fackbook</span></i></a>
+                </div>
+                <div class="instagram">
+                    <a href="https://www.instagram.com/kawe.lay.127/?hl=en"><i class="fab fa-instagram"><span>Instagram</span></i></a>
+                </div>
+            </div>
+            <div class="create">Don't have account?
+                <a href="{{route('signuppage')}}">Create Form</a>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
+@section('script')
+    <script type="text/javascript" src="{{asset('my_asset/custom.js')}}"></script>
+    <script src="https://kit.fontawesome.com/532f1fcad3.js" crossorigin="anonymous"></script>
+@endsection
+
+{{-- <div class="img-fluid">
     <img src="{{asset('frontend_asset/assets/img/detail.jpg')}}" width="1275" height="550">
     <div class="section-title mt-5">
         <h2>Member <span>Login Form</span></h2> 
@@ -10,7 +61,7 @@
 <div class="container my-5">
     <div class="row">
         <div class="col-md-6">
-            <img src="{{asset('frontend_asset/assets/img/login-1.png')}}" width="300" height="300">
+            <img src="{{asset('frontend_asset/assets/img/coffee-bg.jpg')}}" width="400" height="300">
         </div>
         <form method="POST" action="{{route('login')}}" class="my-5">
             @csrf
@@ -48,7 +99,7 @@
             <div class="form-group row">
                 <div class="col-md-6 offset-md-4">
                     <a href="{{route('signuppage')}}" class="text-primary">Create Account</a>
-                    {{-- <div class="form-check">
+                    <div class="form-check">
                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                         <label class="form-check-label" for="remember">
                             {{ __('Remember Me') }}
@@ -58,13 +109,9 @@
                         <a class="btn btn-link" href="{{ route('password.request') }}">
                             {{ __('Forgot Your Password?') }}
                         </a>
-                    @endif --}}
+                    @endif
                 </div>
             </div>
         </form>
     </div>
-</div>
-@endsection
-@section('script')
-    <script type="text/javascript" src="{{asset('my_asset/custom.js')}}"></script>
-@endsection
+</div> --}}
--- resources/views/frontend/signup.blade.php
+++ resources/views/frontend/signup.blade.php
@@ -1,16 +1,78 @@
 @extends('frontendtemplate')
-
+<link rel="stylesheet" type="text/css" href="{{asset('frontend_asset/assets/style.css')}}">
+    
 @section('content')
-<div class="img-fluid">
+<div class="bg-img">
+    <div class="content">
+        <header>Create From</header>
+        <form method="POST" action="{{route('user.store')}}">
+            @csrf
+            <div class="field">
+                <span class="fa fa-user"></span>
+                <input id="name" type="text" class="@error('name') is-invalid @enderror" value="{{ old('name') }}" name="name" autocomplete="name" autofocus placeholder="Your Name" required>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="field space">
+                <span class="fa fa-envelope"></span>
+                <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Your Email" required autocomplete="email">
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="field space">
+                <span class="fa fa-lock"></span>
+                <input id="password" type="password" name="password" class="password @error('password') is-invalid @enderror" placeholder="Your Password" required autocomplete="new-password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="field space">
+                <span class="fa fa-lock"></span>
+                <input id="password-confirm" type="password" name="password_confirmation" class="confirmpassword" placeholder="Your Confirm Password" required autocomplete="new-password">
+            </div>
+            <div class="space">
+                <button type="submit" class="form-control btn btn-primary">
+                    {{ __('Register') }}
+                </button>
+            </div>
+            <div class="link">
+                <div class="facebook">
+                    <a href="https://web.facebook.com/kawe.lay.127/"><i class="fab fa-facebook-f"><span>Fackbook</span></i></a>
+                </div>
+                <div class="instagram">
+                    <a href="https://www.instagram.com/kawe.lay.127/?hl=en"><i class="fab fa-instagram"><span>Instagram</span></i></a>
+                </div>
+            </div>
+            <div class="create">Return To
+                <a href="{{route('signinpage')}}">Login Form</a>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
+@section('script')
+    <script type="text/javascript" src="{{asset('my_asset/custom.js')}}"></script>
+    <script src="https://kit.fontawesome.com/532f1fcad3.js" crossorigin="anonymous"></script>
+@endsection
+
+{{-- <div class="img-fluid">
     <img src="{{asset('frontend_asset/assets/img/detail.jpg')}}" width="1275" height="550">
     <div class="section-title mt-5">
         <h2>Member <span>Register Form</span></h2> 
     </div>
-</div>
-<div class="container my-5">
+</div> --}}
+{{-- <div class="container my-5">
     <div class="row">
         <div class="col-md-6">
-            <img src="{{asset('frontend_asset/assets/img/login-1.png')}}" width="300" height="300">
+            <img src="{{asset('frontend_asset/assets/img/coffee.jpg')}}" width="400" height="300">
         </div>
         <form method="POST" action="{{ route('user.store') }}" class="my-5">
         @csrf
@@ -62,8 +124,4 @@
         </div>
         </form>
     </div>
-</div>
-@endsection
-@section('script')
-    <script type="text/javascript" src="{{asset('my_asset/custom.js')}}"></script>
-@endsection
+</div> --}}
